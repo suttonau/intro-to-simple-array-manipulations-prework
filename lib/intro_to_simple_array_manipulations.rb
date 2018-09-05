d93a87c547dcb5df055723d4f446014e05a74f97
Sean Sutton Jr <sutton_mjbc@icloud.com>
2018-09-05 18:48:56 +0000

    automatically backed up by learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,12 +1,29 @@
 def using_push(array, string)
-  
+  array.push(string)
 end
 
-
-def
+def using_unshift(array, string)
+  array.unshift(string)
 end
 
-def
+def using_pop(array)
+  array.pop
+end
+
+def pop_with_args(array)
+  array.pop(2)
+end
+
+def using_shift(array)
+  array.shift
+end
+
+def shift_with_args(array)
+  array.shift(2)
+end
+
+def using_concat(array, array1)
+  array.concat(array1)
 end
 
 def
